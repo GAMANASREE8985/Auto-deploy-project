7877f8d6bad0c8232541b8e935c04ce4f4aa9e08
GAMANASREE8985 <2200030228@kluniversity.in>
2025-09-12 10:39:03 +0530

    fronetend resolvel

--- 2025-26/Capstone_Project/auto-deploy-project/frontend/src/App.test.js
+++ 2025-26/Capstone_Project/auto-deploy-project/frontend/src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+// src/setupTests.js
+import '@testing-library/jest-dom';
 
 test('renders header', () => {
   render(<App />);
